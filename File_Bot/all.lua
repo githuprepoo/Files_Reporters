eb10cde2714fb30259f44938643d0bc864be184a
WaTaNtEaM <85058247+WaTaNtEaM@users.noreply.github.com>
2021-07-31 20:29:06 +0300

    Add files via upload

--- File_Bot/all.lua
+++ File_Bot/all.lua
@@ -56,6 +56,40 @@ end,nil)
 end,nil)
 end
 end
+if text and (text:match("^@all (.*)$") or text:match("^all (.*)$")) and BasicConstructor(msg) then
+AbsText = text:match("^@all (.*)$") or text:match("^all (.*)$")
+if not database:get(bot_id..'Cick:all'..msg.chat_id_) then
+if database:get(bot_id.."abbas:all:Time"..msg.chat_id_..':'..msg.sender_user_id_) then  
+return 
+send(msg.chat_id_, msg.id_,"*✯︙انتظر 7 دقائق من فضلك من فضلك\n✯︙ثم اعد المحاوله بعدها*")
+end
+database:setex(bot_id..'abbas:all:Time'..msg.chat_id_..':'..msg.sender_user_id_,300,true)
+tdcli_function({ID="GetChannelFull",channel_id_ = msg.chat_id_:gsub('-100','')},function(argg,dataa) 
+tdcli_function({ID = "GetChannelMembers",channel_id_ = msg.chat_id_:gsub('-100',''), offset_ = 0,limit_ = dataa.member_count_},function(ta,amir)
+x = 0
+tags = 0
+local list = amir.members_
+for k, v in pairs(list) do
+tdcli_function({ID="GetUser",user_id_ = v.user_id_},function(arg,data)
+if x == 5 or x == tags or k == 0 then
+tags = x + 5
+t = "#all : "..AbsText.."\n\n"
+end
+x = x + 1
+tagname = data.first_name_
+tagname = tagname:gsub("]","")
+tagname = tagname:gsub("[[]","")
+t = t..", ["..tagname.."](tg://user?id="..v.user_id_..")"
+if x == 5 or x == tags or k == 0 then
+local Text = t:gsub('#all,','#all\n')
+sendText(msg.chat_id_,Text,msg.id_/2097152/0.5,'md')
+end
+end,nil)
+end
+end,nil)
+end,nil)
+end
+end
 if text == 'تفعيل all' and CoSu(msg) or text == 'تفعيل @all' and CoSu(msg) then
 if database:get(bot_id..'Cick:all'..msg.chat_id_) then
 Text = ' *✯︙تم تفعيل امر @all*'
